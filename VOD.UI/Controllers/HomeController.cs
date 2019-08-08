1c01734090308421250fc240b22c7f6645608a62
andreitch <andreitch.top@gmail.com>
2019-08-08 19:33:34 +0300

    Scaffold Login and Register Razor Pages. Changed the layout of the Login page, and applied CSS and Bootstrap classes to its elements, to make it look nicer to the user.

--- VOD.UI/Controllers/HomeController.cs
+++ VOD.UI/Controllers/HomeController.cs
@@ -4,14 +4,27 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using VOD.Common.Entities;
 using VOD.UI.Models;
 
 namespace VOD.UI.Controllers
 {
     public class HomeController : Controller
     {
+        private SignInManager<VODUser> _signInManager;
+
+        public HomeController(SignInManager<VODUser> signInMgr)
+        {
+            _signInManager = signInMgr;
+        }
+
         public IActionResult Index()
         {
+            if (!_signInManager.IsSignedIn(User))
+                return RedirectToPage("/Account/Login",
+                    new { Area = "Identity" });
+
             return View();
         }
 
